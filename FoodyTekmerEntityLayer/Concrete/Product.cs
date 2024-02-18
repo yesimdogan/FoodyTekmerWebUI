b8ef17a497cd84860f73c2157d2f89f23d89ab92
Yeşim Doğan <yesimdevrimdogan@gmail.com>
2024-02-18 21:05:21 +0300

    Add project files.

---
+++ FoodyTekmerEntityLayer/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodyTekmerEntityLayer.Concrete;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public string ImageUrl { get; set; } 
+    public decimal OldPrice { get; set; }    
+    public decimal NewPrice { get; set; }
+    public bool Status { get; set; } 
+}
